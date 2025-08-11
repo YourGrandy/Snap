9a1a053ec2b91ad132194039ccabc20537bbf898
Seka <sekastrashes@gmail.com>
2025-08-11 15:35:58 +0300

    Fixing user's avatars on the right sidebar

--- src/components/shared/UserCard.tsx
+++ src/components/shared/UserCard.tsx
@@ -7,25 +7,31 @@ type UserCardProps = {
   user: Models.Document;
 };
 
+const buildFileDownloadUrl = (fileId: string) => {
+  if (!fileId) return "";
+  return `https://fra.cloud.appwrite.io/v1/storage/buckets/689628660035b0ddbe53/files/${fileId}/download?project=6896256c0023d71c5cff`;
+};
+
 const UserCard = ({ user }: UserCardProps) => {
   return (
     <Link to={`/profile/${user.$id}`} className="user-card">
       <img
-        src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+        // src={user.imageUrl || "/assets/icons/profile-placeholder.svg"}
+        src={user.imageId ? buildFileDownloadUrl(user.imageId) : user.imageUrl}
         alt="creator"
-        className="rounded-full w-14 h-14"
+        className="object-cover rounded-full w-14 h-14"
       />
 
-      <div className="flex-center flex-col gap-1">
-        <p className="base-medium text-light-1 text-center line-clamp-1">
+      <div className="flex-col gap-1 flex-center">
+        <p className="text-center base-medium text-light-1 line-clamp-1">
           {user.name}
         </p>
-        <p className="small-regular text-light-3 text-center line-clamp-1">
+        <p className="text-center small-regular text-light-3 line-clamp-1">
           @{user.username}
         </p>
       </div>
 
-      <Button type="button" size="sm" className="shad-button_primary px-5">
+      <Button type="button" size="sm" className="px-5 shad-button_primary">
         Follow
       </Button>
     </Link>
